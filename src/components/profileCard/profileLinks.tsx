5ae288849f39832e882acc01dff4d7999ed142b9
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-03-17 00:47:47 -0400

    working on hover reveals for links

--- src/components/profileCard/profileLinks.tsx
+++ src/components/profileCard/profileLinks.tsx
@@ -4,23 +4,37 @@ import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+
+const HoverableIcon = (props) => {
+
+    const { value, icon } = props;
+
+    return (
+        <div className="hover-icon">
+            <FontAwesomeIcon icon={icon} />
+            <div className="hidden-value">{value}</div>
+        </div>
+    )
+}
+
 export const ProfileLinks = (props: any) => {
 
     const { connections } = props;
+    const { phoneNumber, email, linkedIn, gitHub } = connections;
 
     return (
         <div className="profile-links">
             {/** Phone icon link */}
-            {connections.phoneNumber && <FontAwesomeIcon icon={faPhoneAlt} />}
+            {phoneNumber && <HoverableIcon icon={faPhoneAlt} value={phoneNumber} />}
 
             {/** Email icon link */}
-            {connections.email && <FontAwesomeIcon icon={faEnvelope} />}
+            {email && <HoverableIcon icon={faEnvelope} value={email} />}
 
             {/** LinkedIn icon link */}
-            {connections.linkedIn && <FontAwesomeIcon icon={faLinkedinIn} />}
+            {linkedIn && <HoverableIcon icon={faLinkedinIn} value={linkedIn} />}
 
             {/** GitHub icon link */}
-            {connections.gitHub && <FontAwesomeIcon icon={faGithub} />}
+            {gitHub && <HoverableIcon icon={faGithub} value={gitHub} />}
 
         </div>
     );
