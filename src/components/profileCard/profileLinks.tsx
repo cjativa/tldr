38f4cf6b75097c69c728eb0a1c89c6cebeee290c
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-03-23 20:17:33 -0400

    stagger mouse leave

--- src/components/profileCard/profileLinks.tsx
+++ src/components/profileCard/profileLinks.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import { faEnvelope, faPhoneAlt, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -10,18 +10,13 @@ const HoverableIcon = (props) => {
     const { value, icon, displaceRight } = props;
     const positionStyle = displaceRight ? 'right' : 'left';
 
-    const [expand, setExpand] = useState(false);
-
-    const toggleExpand = () => {
-        setExpand(!expand);
-    }
-
-    const expandClass = (expand) ? 'expand' : 'not-expand';
+    const spanRef = useRef(null);
+    const [spanWidth, setSpanWidth] = useState(0);
 
     return (
         <div className={`hover-container ${positionStyle}`}>
-            <div className={`hover-icon`}><FontAwesomeIcon icon={icon} /></div>
-            <div className={`hidden-value ${expandClass}`}>{value}</div>
+            <div className={`hover-icon`} onMouseEnter={() => setSpanWidth(spanRef.current.offsetWidth)}><FontAwesomeIcon onMouseLeave={() => setTimeout(() => setSpanWidth(0), 200)} icon={icon} /></div>
+            <div className={`hidden-value`} style={{ width: spanWidth }}><span style={{ width: spanWidth }} ref={spanRef}>{value}</span></div>
         </div >
     )
 }
